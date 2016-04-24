59bbf6afe12bafeec80ff372799d7dd4333a7414
Patrick Reynolds <patrick.reynolds@e-smartz.com>
2016-04-24 19:57:51 +0100

    Initial Commit

---
+++ src/mode.js
@@ -0,0 +1,156 @@
+/**
+ * Copyright 2015 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+/**
+ * @typedef {{
+ *   localDev: boolean,
+ *   development: boolean,
+ *   minified: boolean,
+ *   test: boolean,
+ *   log: (string|undefined),
+ *   version: string,
+ * }}
+ */
+let ModeDef;
+
+/** @typedef {?ModeDef} */
+let mode = null;
+
+/** @typedef {string} */
+const version = '$internalRuntimeVersion$';
+
+/**
+ * `fullVersion` is the prefixed version we serve off of the cdn.
+ * The prefix denotes canary(00) or prod(01) or an experiment version ( > 01).
+ * @type {string}
+ */
+let fullVersion = '';
+
+/**
+ * Provides info about the current app.
+ * @return {!ModeDef}
+ */
+export function getMode() {
+  if (mode) {
+    return mode;
+  }
+  return mode = getMode_();
+}
+
+/**
+ * Set mode in a test. Pass null in afterEach function to reset.
+ * @param {?ModeDef} m
+ */
+export function setModeForTesting(m) {
+  mode = m;
+}
+
+/**
+ * Provides info about the current app.
+ * @return {!ModeDef}
+ */
+function getMode_() {
+  if (window.context && window.context.mode) {
+    return window.context.mode;
+  }
+  const isLocalDev = (location.hostname == 'localhost' ||
+      (location.ancestorOrigins && location.ancestorOrigins[0] &&
+          location.ancestorOrigins[0].indexOf('http://localhost:') == 0)) &&
+      // Filter out localhost running against a prod script.
+      // Because all allowed scripts are ours, we know that these can only
+      // occur during local dev.
+      !!document.querySelector('script[src*="/dist/"],script[src*="/base/"]');
+
+  const developmentQuery = parseQueryString_(
+      // location.originalHash is set by the viewer when it removes the fragment
+      // from the URL.
+      location.originalHash || location.hash);
+
+  if (!fullVersion) {
+    fullVersion = getFullVersion_(window, isLocalDev);
+  }
+
+  return {
+    localDev: isLocalDev,
+    // Triggers validation
+    development: developmentQuery['development'] == '1' || window.AMP_DEV_MODE,
+    minified: process.env.NODE_ENV == 'production',
+    test: window.AMP_TEST,
+    log: developmentQuery['log'],
+    version: fullVersion,
+  };
+}
+
+/**
+ * Parses the query string of an URL. This method returns a simple key/value
+ * map. If there are duplicate keys the latest value is returned.
+ * @param {string} queryString
+ * @return {!Object<string, string>}
+ * TODO(dvoytenko): dedupe with `url.js:parseQueryString`. This is currently
+ * necessary here because `url.js` itself inderectly depends on `mode.js`.
+ */
+function parseQueryString_(queryString) {
+  const params = Object.create(null);
+  if (!queryString) {
+    return params;
+  }
+  if (queryString.indexOf('?') == 0 || queryString.indexOf('#') == 0) {
+    queryString = queryString.substr(1);
+  }
+  const pairs = queryString.split('&');
+  for (let i = 0; i < pairs.length; i++) {
+    const pair = pairs[i];
+    const eqIndex = pair.indexOf('=');
+    let name;
+    let value;
+    if (eqIndex != -1) {
+      name = decodeURIComponent(pair.substring(0, eqIndex)).trim();
+      value = decodeURIComponent(pair.substring(eqIndex + 1)).trim();
+    } else {
+      name = decodeURIComponent(pair).trim();
+      value = '';
+    }
+    if (name) {
+      params[name] = value;
+    }
+  }
+  return params;
+}
+
+/**
+ * Retrieve the `fullVersion` which will have a numeric prefix
+ * denoting canary/prod/experiment.
+ *
+ * @param {!Window} win
+ * @param {boolean} isLocalDev
+ * @return {string}
+ * @private
+ * @visibleForTesting
+ */
+export function getFullVersion_(win, isLocalDev) {
+  // If it's local dev then we won't actually have a full version so
+  // just use the version.
+  if (isLocalDev) {
+    return version;
+  }
+
+  if (win.AMP_CONFIG && win.AMP_CONFIG.v) {
+    return win.AMP_CONFIG.v;
+  }
+
+  return version;
+}
